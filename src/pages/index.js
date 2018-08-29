4e3c06587d65551e1a7d28dbfa2e4714147f7f38
rob-now <rnowakowski.prv@gmail.com>
2018-08-29 09:15:42 +0200

    Installed contentful and connection established

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,48 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Link from 'gatsby-link'
+import { createClient } from 'contentful'
+import contentfulConfig from '../../.contentful.json'
 
-const IndexPage = () => (
-  <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
-  </div>
-)
+class IndexPage extends Component {
+  state = {
+    items: [],
+  }
+
+  componentDidMount() {
+    const client = createClient({
+      space: contentfulConfig.spaceId,
+      accessToken: contentfulConfig.accessToken,
+    })
+
+    client
+      .getEntries()
+      .then(response => {
+        console.log(response.items)
+        this.setState({
+          items: response.items,
+        })
+      })
+      .catch(console.error)
+  }
+
+  render() {
+    if (!this.state.items.length) return <p>No items found.</p>
+
+    return (
+      <div>
+        {/* {this.state.items.map(item => {
+          console.log(item)
+          return (
+            <div>
+              <h2>{item.fields.title}</h2>
+              <h3>{item.fields.tile1Title}</h3>
+              <p>{item.fields.tile1Description}</p>
+            </div>
+          )
+        })} */}
+      </div>
+    )
+  }
+}
 
 export default IndexPage
