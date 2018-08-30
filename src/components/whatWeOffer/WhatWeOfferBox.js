e7eb7fe9dedbd8a9c349ecd6f50330fffb787b55
rob-now <rnowakowski.prv@gmail.com>
2018-08-30 19:16:10 +0200

    What we offer section media querries

--- src/components/whatWeOffer/WhatWeOfferBox.js
+++ src/components/whatWeOffer/WhatWeOfferBox.js
@@ -15,9 +15,16 @@ const StyledWhatWeOfferBox = styled.div`
         ? `margin: 0 0 0 28px;`
         : null};
 
-  // @media screen and (max-width: 1024px) {
-  //   margin: 30px;
-  // }
+  @media screen and (max-width: 1199.9px) {
+    margin: 30px;
+  }
+
+  @media screen and (max-width: 575.9px) {
+    flex-direction: column;
+    justify-content: center;
+    height: 300px;
+    margin: 28px 0;
+  }
 `
 
 const StyledWwoBoxIcon = styled.img`
@@ -28,6 +35,10 @@ const StyledH3 = styled.h3`
   font-size: 18px;
   font-weight: bold;
   color: #333;
+
+  @media screen and (max-width: 575.9px) {
+    margin-top: 1.45rem;
+  }
 `
 
 const StyledParagraph = styled.p`
@@ -38,6 +49,10 @@ const StyledParagraph = styled.p`
 
 const StyledWwoBoxContent = styled.div`
   margin-right: 28px;
+
+  @media screen and (max-width: 575.9px) {
+    margin-left: 28px;
+  }
 `
 
 export default ({ id, img, contentfulItems, titleId, descriptionId }) => {
