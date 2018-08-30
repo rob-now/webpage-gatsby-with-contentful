1e99110562aac68c919a8a26c7ffb374c4578810
rob-now <rnowakowski.prv@gmail.com>
2018-08-30 10:38:58 +0200

    Extracted read more button as component

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -2,6 +2,7 @@ import React, { Component, Fragment } from 'react'
 import Nav from '../nav/Nav'
 import HeroTitle from './HeroTitle'
 import styled from 'styled-components'
+import ReadMoreButton from './ReadMoreButton'
 
 const HeroContainer = styled.div`
   background: #122636;
@@ -51,53 +52,6 @@ const StyledHeroBoxBottom = styled.div`
   background: #fff;
 `
 
-const StyledReadMoreButton = styled.button`
-  border: none;
-  background: none;
-  display: flex;
-  align-items: center;
-  position: absolute;
-  bottom: 0;
-  right: 0;
-  &:hover {
-    cursor: pointer;
-  }
-`
-
-const StyledReadMoreText = styled.span`
-  width: 135px;
-  height: 34px;
-  line-height: 34px;
-  font-size: 13px;
-  font-weight: bold;
-  text-transform: uppercase;
-  background: #1b2936;
-  color: #fff;
-  transition: background 0.3s;
-
-  ${({ hovered }) =>
-    hovered &&
-    `
-      background: #006db7;
-      transition: background 0.3s;
-    `};
-`
-
-const StyledReadMoreArrow = styled.span`
-  width: 30px;
-  height: 34px;
-  line-height: 30px;
-  background: #2f3840;
-  transition: background 0.3s;
-
-  ${({ hovered }) =>
-    hovered &&
-    `
-      background: #037ed1;
-      transition: background 0.3s;
-    `};
-`
-
 class Hero extends Component {
   state = {
     isReadMoreButtonHovered: false,
@@ -126,33 +80,11 @@ class Hero extends Component {
             <StyledHeroBoxesContainer>
               <StyledHeroBox>
                 <StyledHeroBoxTop>
-                  <StyledReadMoreButton
-                    type="button"
-                    onPointerEnter={this.handleMouseHover}
-                    onPointerLeave={this.handleMouseHover}
-                  >
-                    <StyledReadMoreText hovered={isReadMoreButtonHovered}>
-                      Read more
-                    </StyledReadMoreText>
-                    <StyledReadMoreArrow hovered={isReadMoreButtonHovered}>
-                      <svg
-                        aria-hidden="true"
-                        data-prefix="fas"
-                        data-icon="angle-right"
-                        width="6px"
-                        height="9px"
-                        class="svg-inline--fa fa-angle-right fa-w-8"
-                        role="img"
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 256 512"
-                      >
-                        <path
-                          fill="#fff"
-                          d="M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34z"
-                        />
-                      </svg>
-                    </StyledReadMoreArrow>
-                  </StyledReadMoreButton>
+                  <ReadMoreButton
+                    text="Read more"
+                    isReadMoreButtonHovered={isReadMoreButtonHovered}
+                    handleMouseHover={this.handleMouseHover}
+                  />
                 </StyledHeroBoxTop>
                 <StyledHeroBoxBottom>Bottom</StyledHeroBoxBottom>
               </StyledHeroBox>
---
+++ src/components/hero/ReadMoreButton.js
@@ -0,0 +1,81 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const StyledReadMoreButton = styled.button`
+  border: none;
+  background: none;
+  display: flex;
+  align-items: center;
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  &:hover {
+    cursor: pointer;
+  }
+`
+
+const StyledReadMoreText = styled.span`
+  width: 135px;
+  height: 34px;
+  line-height: 34px;
+  font-size: 13px;
+  font-weight: bold;
+  text-transform: uppercase;
+  background: #1b2936;
+  color: #fff;
+  transition: background 0.3s;
+
+  ${({ hovered }) =>
+    hovered &&
+    `
+      background: #006db7;
+      transition: background 0.3s;
+    `};
+`
+
+const StyledReadMoreArrow = styled.span`
+  width: 30px;
+  height: 34px;
+  line-height: 30px;
+  background: #2f3840;
+  transition: background 0.3s;
+
+  ${({ hovered }) =>
+    hovered &&
+    `
+      background: #037ed1;
+      transition: background 0.3s;
+    `};
+`
+
+export default ({ text, isReadMoreButtonHovered, handleMouseHover }) => {
+  return (
+    <StyledReadMoreButton
+      type="button"
+      onPointerEnter={handleMouseHover}
+      onPointerLeave={handleMouseHover}
+    >
+      <StyledReadMoreText hovered={isReadMoreButtonHovered}>
+        {text}
+      </StyledReadMoreText>
+      <StyledReadMoreArrow hovered={isReadMoreButtonHovered}>
+        <svg
+          aria-hidden="true"
+          data-prefix="fas"
+          data-icon="angle-right"
+          width="6px"
+          height="9px"
+          class="svg-inline--fa fa-angle-right fa-w-8"
+          role="img"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 256 512"
+        >
+          <path
+            fill="#fff"
+            d="M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34z"
+          />
+        </svg>
+      </StyledReadMoreArrow>
+    </StyledReadMoreButton>
+  )
+}
